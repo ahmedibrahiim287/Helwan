a7f122c5181b56b5ec75ceb27d6c2fab7688982f
ahmedibrahiim287 <49068254+ahmedibrahiim287@users.noreply.github.com>
2021-03-20 22:21:35 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Home }from './screens/index';
+import {Contact} from './component/index';
+
+function App() {
+  return (
+    <div className="App">
+      <Contact></Contact>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/contact.js
@@ -0,0 +1,14 @@
+//contact form (name + email + phone + comments)
+import React, { Component } from "react";
+
+export class contact extends Component {
+  render() {
+    return (
+        <div>
+<text>ffffffff</text>
+        </div>
+    );
+}
+}
+
+export default contact;
